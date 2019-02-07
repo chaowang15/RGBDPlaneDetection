5641a2aef67f5b7be79205fccae08cdaa5a55bad
cwang <cwang@matterport.com>
2019-02-07 11:47:19 -0800

    add figures

--- demo_linux.sh
+++ demo_linux.sh
@@ -17,12 +17,13 @@ cecho() {
   echo "$text"
 }
 
+RGBD="/home/chao/dev/data/bundlefusion/copyroom/copyroom/"
 # RGBD="/d/3drecon/data/3dlite/apt/apt-rgbd-render/"
 # RGBD="/d/3drecon/data/bundlefusion/copyroom/copyroom/"
 # OUTPUT="/d/3drecon/data/bundlefusion/copyroom/copyroom-plane-temp/"
 # ORIGINALRGBD="/d/3drecon/data/bundlefusion/copyroom/copyroom/"
-RGBD="/media/chao/768ACA058AC9C23B/3drecon/data/bundlefusion/office0/office0_temp/office0_temp-original/"
-OUTPUT=$RGBD
+# RGBD="/media/chao/768ACA058AC9C23B/3drecon/data/bundlefusion/office0/office0_temp/office0_temp-original/"
+OUTPUT="/home/chao/dev/data/bundlefusion/copyroom/planes-all-frames"
 if [ ! -d "$OUTPUT" ]; then
   mkdir $OUTPUT
 fi
@@ -34,16 +35,16 @@ cecho g "RGBDPlaneDetection code: $code"
 #     echo $file >> list.txt
 #     python $genply 
 # done
-num=400
+num=4479
 for i in  $(seq -f "%06g" 0 $num)
 do
 	cecho g "Reading frame $i"
   # $code ${RGBD}frame-$i-color.png ${RGBD}frame-$i-depth.png $OUTPUT
-  # $code -o ${RGBD}frame-$i.color.jpg ${RGBD}frame-$i.depth.png $OUTPUT
-  $code -o ${RGBD}frame-$i.rendered-color.png ${RGBD}frame-$i.rendered-depth.png $OUTPUT
+  $code -o ${RGBD}frame-$i.color.jpg ${RGBD}frame-$i.depth.png $OUTPUT
+  # $code -o ${RGBD}frame-$i.rendered-color.png ${RGBD}frame-$i.rendered-depth.png $OUTPUT
 	# $code -o ${RGBD}frame-$i-color.png ${RGBD}frame-$i-depth.png
-  cecho g "Copying pose file of frame $i"
-  # cp ${ORIGINALRGBD}frame-$i.pose.txt ${OUTPUT}frame-$i-pose.txt
+  # cecho g "Copying pose file of frame $i"
+  # cp ${RGBD}frame-$i.pose.txt ${OUTPUT}frame-$i-pose.txt
 done
 
 
